d6306fffc2eb494936ceadaf4d7a02c769ff4f4a
developeranaz <71500526+developeranaz@users.noreply.github.com>
2021-05-26 13:46:35 +0530

    Update app.py

--- web/app.py
+++ web/app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+#title
 st.title("Hello This is developeranaz")
+#Markdown
+st.write("""
+# You can Start Forking /Cloning and  Build your own webapp
+Don't Forget to star my repo
+""")
